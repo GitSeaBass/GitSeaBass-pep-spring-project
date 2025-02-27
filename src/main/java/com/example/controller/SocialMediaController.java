9a7eed2cf58e77cc45c9e15272163a1566dc1905
Sebastian Garcia <sag44402@uga.edu>
2025-02-27 16:37:37 +0000

    passed getAllMessages tests

--- src/main/java/com/example/controller/SocialMediaController.java
+++ src/main/java/com/example/controller/SocialMediaController.java
@@ -1,5 +1,14 @@
 package com.example.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.entity.Message;
+import com.example.service.MessageService;
 
 /**
  * TODO: You will need to write your own endpoints and handlers for your controller using Spring. The endpoints you will need can be
@@ -7,6 +16,20 @@ package com.example.controller;
  * where applicable as well as the @ResponseBody and @PathVariable annotations. You should
  * refer to prior mini-project labs and lecture materials for guidance on how a controller may be built.
  */
+@RestController
 public class SocialMediaController {
+    private MessageService messageService;
 
+    @Autowired
+    public SocialMediaController(MessageService messageService) {
+        this.messageService = messageService;
+    }
+
+    @GetMapping("messages")
+    public ResponseEntity<List<Message>> getAllMessages() {
+        return ResponseEntity.status(200).body(messageService.getAllMessages());
+    }
+
+    @GetMapping("messages/{message_id}")
+    public ResponseEntity<Message>
 }
